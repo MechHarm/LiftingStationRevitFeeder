940d5325f530cdb7f77a1aa8ec24c5a6289de226
Attila HARMATI <aharmati@transcendinfra.com>
2024-07-04 20:36:36 +0200

    First batch successfully tested in revit.

--- LiftingStationRevitFeeder.Domain.Test/LiftingStationRevitFeederTest.cs
+++ LiftingStationRevitFeeder.Domain.Test/LiftingStationRevitFeederTest.cs
@@ -26,6 +26,38 @@ namespace LiftingStationRevitFeeder.Domain.Test
             sut.PumpInletVelocity.GetValue().Should().BeApproximately(2, 0.001);
             sut.DN1.GetValue().Should().Be(250);
             sut.DN2.GetValue().Should().Be(250);
+            sut.DN3.GetValue().Should().Be(250);
+            sut.DN4.GetValue().Should().Be(300);
+            sut.DN5.GetValue().Should().Be(250);
+            sut.DNBreath.GetValue().Should().Be(250);
+            sut.DNInlet.GetValue().Should().Be(550);
+            sut.DNBackflow.GetValue().Should().Be(250);
+            sut.DimA.GetValue().Should().Be(250);
+            sut.DimB.GetValue().Should().Be(1700);
+            sut.DimC.GetValue().Should().Be(425);
+            sut.DimD.GetValue().Should().Be(865);
+            sut.DimE.GetValue().Should().Be(720);
+            sut.DimF.GetValue().Should().Be(170);
+            sut.DimG.GetValue().Should().Be(420);
+            sut.DimH.GetValue().Should().Be(750);
+            sut.DimI.GetValue().Should().Be(450);
+            sut.DimJ.GetValue().Should().Be(150);
+            sut.DimK.GetValue().Should().Be(800);
+            sut.DimL.GetValue().Should().Be(800);
+            sut.DimM.GetValue().Should().Be(375);
+            sut.DimN.GetValue().Should().Be(1000);
+            sut.DimO.GetValue().Should().Be(860);
+            sut.DimP.GetValue().Should().Be(1220);
+            sut.DimQ.GetValue().Should().Be(860);
+            sut.DimR.GetValue().Should().Be(960);
+            sut.DimS.GetValue().Should().Be(450);
+            sut.DimT.GetValue().Should().Be(1110);
+            sut.DimU.GetValue().Should().Be(400);
+            sut.DimV.GetValue().Should().Be(400);
+            sut.DimZ.GetValue().Should().Be(9300);
+            sut.DimW.GetValue().Should().Be(5700);
+            sut.MinLSWallDistanceX.GetValue().Should().Be(7000);
+            sut.MinLSWallDistanceY.GetValue().Should().Be(2860);
         }
         [Test]
         public void LiftingStationRevitFeederTest_WhenBaseparametersWithoutOptionals_ExpectedValuesAreSet()
@@ -44,6 +76,38 @@ namespace LiftingStationRevitFeeder.Domain.Test
             sut.PumpInletVelocity.GetValue().Should().BeApproximately(1.7, 0.001);
             sut.DN1.GetValue().Should().Be(150);
             sut.DN2.GetValue().Should().Be(150);
+            sut.DN3.GetValue().Should().Be(150);
+            sut.DN4.GetValue().Should().Be(150);
+            sut.DN5.GetValue().Should().Be(125);
+            sut.DNBreath.GetValue().Should().Be(150);
+            sut.DNInlet.GetValue().Should().Be(250);
+            sut.DNBackflow.GetValue().Should().Be(150);
+            sut.DimA.GetValue().Should().Be(200);
+            sut.DimB.GetValue().Should().Be(1200);
+            sut.DimC.GetValue().Should().Be(325);
+            sut.DimD.GetValue().Should().Be(585);
+            sut.DimE.GetValue().Should().Be(520);
+            sut.DimF.GetValue().Should().Be(120);
+            sut.DimG.GetValue().Should().Be(270);
+            sut.DimH.GetValue().Should().Be(600);
+            sut.DimI.GetValue().Should().Be(325);
+            sut.DimJ.GetValue().Should().Be(85);
+            sut.DimK.GetValue().Should().Be(800);
+            sut.DimL.GetValue().Should().Be(800);
+            sut.DimM.GetValue().Should().Be(225);
+            sut.DimN.GetValue().Should().Be(1000);
+            sut.DimO.GetValue().Should().Be(720);
+            sut.DimP.GetValue().Should().Be(440);
+            sut.DimQ.GetValue().Should().Be(720);
+            sut.DimR.GetValue().Should().Be(885);
+            sut.DimS.GetValue().Should().Be(225);
+            sut.DimT.GetValue().Should().Be(745);
+            sut.DimU.GetValue().Should().Be(400);
+            sut.DimV.GetValue().Should().Be(400);
+            sut.DimZ.GetValue().Should().Be(4400);
+            sut.DimW.GetValue().Should().Be(4565);
+            sut.MinLSWallDistanceX.GetValue().Should().Be(3600);
+            sut.MinLSWallDistanceY.GetValue().Should().Be(2240);
         }
     }
 }
--- LiftingStationRevitFeeder.Domain/RevitFeed.cs
+++ LiftingStationRevitFeeder.Domain/RevitFeed.cs
@@ -58,6 +58,7 @@ namespace LiftingStationRevitFeeder.Domain
         public Length? DimV { get; private set; }
         public Length? DimX { get; private set; }
         public Length? DimY { get; private set; }
+        public Length? DimZ { get; private set; }
         public Length? DimW { get; private set; }
         public Length MinLSWallDistanceX { get; private set; }
         public Length MinLSWallDistanceY { get; private set; }
@@ -122,29 +123,121 @@ namespace LiftingStationRevitFeeder.Domain
             DesignPeakHourFlow = designPeakHourFlow;
             Head = head;
             PumpInletVelocity = pumpInletVelocity ?? new Velocity(1.7, "m s-1");
-            //PressurizedPipeVelocity = pressurizedPipeVelocity ?? new Velocity(2, "m h-1");
-            //GravityPipeVelocity = gravityPipeVelocity ?? new Velocity(0.6, "m h-1");
+            PressurizedPipeVelocity = pressurizedPipeVelocity ?? new Velocity(2, "m s-1");
+            GravityPipeVelocity = gravityPipeVelocity ?? new Velocity(0.6, "m s-1");
             DutyPumpsCount = dutyPumpsCount ?? 1;
             StandbyPumpsCount = standbyPumpsCount ?? 1;
             Flow = DesignPeakHourFlow / DutyPumpsCount.Value;
             NumberOfPumps = DutyPumpsCount.Value + StandbyPumpsCount.Value;
             DN1 = dn1 ?? GetSuctionDiameter();
             DN2 = dn2 ?? GetDischargeDiameter(DN1);
-            //DN3 = dn3 ?? GetFeederDiameter();
-            //DN4 = dn4 ?? GetRisingMainDiameter();
-            //DN5 = dn5 ?? GetFlowmeterDiameter();
+            DN3 = dn3 ?? GetFeederDiameter();
+            DN4 = dn4 ?? GetRisingMainDiameter();
+            DN5 = dn5 ?? GetFlowmeterDiameter();
+            DNBreath = dnBreath ?? DN2;
+            DNInlet = dnInlet ?? GetGravityMainDiameter();
+            DNBackflow = dnBackflow ?? DN2;
+            Freeboard = freeboard ?? new Length(500, "mm");
+            LevA = levA ?? new Length(1000, "mm");
+            LevB = levB ?? new Length(500, "mm");
+            LevC = levC ?? new Length(500, "mm");
+            LevD = levD ?? new Length(500, "mm");
+            LevE = levE ?? new Length(500, "mm");
+            LevF = levF ?? new Length(500, "mm");
+            LevG = levG ?? new Length(500, "mm");
+            LevH = levH ?? new Length(500, "mm");
+            LevI = levI ?? new Length(500, "mm");
+            WetWellDepth = LevA + LevB + LevC + LevD + LevE + LevF + LevG + LevH + LevI;
+            DimA = dimA ?? GetPumpDimensionA();
+            DimB = dimB ?? GetPumpDimensionB();
+            DimC = dimC ?? GetPumpDimensionC();
+            DimD = dimD ?? GetPumpDimensionD();
+            DimF = dimF ?? GetPumpDimensionF();
+            DimG = dimG ?? DimF + DN2;
+            DimH = dimH ?? GetPumpDimensionH();
+            DimI = dimI ?? GetPumpDimensionI();
+            DimE = dimE ?? DimI * 1.6;
+            DimJ = dimJ ?? GetPumpDimensionJ();
+            DimK = dimK ?? new Length(800, "mm");
+            DimL = dimL ?? new Length(800, "mm");
+            DimM = dimM ?? DN3 * 1.5;
+            DimN = dimN ?? new Length(1000, "mm");
+            DimO = dimO ?? GetPumpDimensionO();
+            DimP = dimP ?? GetPumpDimensionP();
+            DimQ = dimQ ?? DimO;
+            DimR = dimR ?? GetPumpDimensionR();
+            DimS = dimS ?? DN4 * 1.5;
+            DimT = dimT ?? GetPumpDimensionT();
+            DimU = dimU ?? new Length(400, "mm");
+            DimV = dimV ?? new Length(400, "mm");
+            MinLSWallDistanceX = GetMinimumLSWallDistanceX();
+            MinLSWallDistanceY = GetMinimumLSWallDistanceY();
+            //DimX = dimX ?? Civil Requirement > MinLSWallDistanceX
+            //DimY = dimY ?? Civil Requirement > MinLSWallDistanceY
+            DimZ = dimZ ?? GetPumpDimensionZ();
+            DimW = dimW ?? DimN + DimO + DimP + DimQ + DimU + DimV + DimR;
+            DimX = DimW;
         }
 
 
         private Length GetSuctionDiameter() =>
             new Length(new Length(Math.Max(0.065, Math.Sqrt((4 * Flow.GetValue("m3 s-1")
                 / PumpInletVelocity.GetValue("m s-1") / Math.PI)))).CopyConvertTo("mm")).GetStandardDiameter();
-        
+
         private static Length? GetDischargeDiameter(Length suctionDiameter) => suctionDiameter < new Length(150, "mm")
                 ? suctionDiameter.GetLastSmallerDiameter()
                 : new Length(Math.Min(suctionDiameter.GetValue("mm"), 400), "mm");
-        //private Length GetFeederDiameter() =>
-        //    new Length(new Length(Math.Max(0.05, Math.Sqrt((4 * Flow.GetValue("m3 h-1") / 3600 / PressurizedPipeVelocity.GetValue("m s-1") / Math.PI)))).CopyConvertTo("mm")).GetStandardDiameter();
+        private Length GetFeederDiameter() =>
+            new Length(new Length(Math.Max(DN2.CopyConvertTo("m").Value, Math.Sqrt((4 * Flow.GetValue("m3 s-1")
+                / PressurizedPipeVelocity.GetValue("m s-1") / Math.PI)))).CopyConvertTo("mm")).GetStandardDiameter();
+        private Length GetRisingMainDiameter() =>
+            new Length(new Length(Math.Max(DN3.CopyConvertTo("m").Value, Math.Sqrt((4 * DesignPeakHourFlow.GetValue("m3 s-1")
+                / PressurizedPipeVelocity.GetValue("m s-1") / Math.PI)))).CopyConvertTo("mm")).GetStandardDiameter();
+        private Length GetFlowmeterDiameter() =>
+           new Length(DN4).GetLastSmallerDiameter();
+        private Length GetGravityMainDiameter() =>
+            new Length(new Length(Math.Min(0.6, Math.Max(0.05, Math.Sqrt((4 * DesignPeakHourFlow.GetValue("m3 s-1")
+                / GravityPipeVelocity.GetValue("m s-1") / Math.PI))))).CopyConvertTo("mm")).GetStandardDiameter();
+        private Length GetPumpDimensionA() => DN1.Value == 150 ? new Length(200, "mm")
+            : new Length(Math.Min(400, Math.Max(100, DN1.Value)), "mm");
+        // private Length GetPumpDimensionB() =>
+        //    new Length(Math.Min(2400, 8 * DN1.Value), "mm");
+        private Length GetPumpDimensionB() => Math.Floor(DN1.Value / 100) == 2 ? new Length(DN1.Value * 8 - 300, "mm")
+                                            : DN1.Value < 200 ? new Length(DN1.Value * 8, "mm")
+                                            : new Length(2400, "mm");
+        private Length GetPumpDimensionC() =>
+            new Length(DN2.Value / 2 + DimA.Value + 50, "mm");
+        private Length GetPumpDimensionD() =>
+            new Length(DimC.Value + Math.Round(DN2.Value * 0.35) * 5, "mm");
+        private Length GetPumpDimensionF() =>
+            new Length(Math.Round(((DN2.Value + 90)/2)/5) * 5, "mm");
+        private Length GetPumpDimensionH() => DN1.Value < 100 ? new Length(DN1.Value * 5, "mm")
+                                            : DN1.Value < 175 ? new Length(DN1.Value * 4, "mm")
+                                            : DN1.Value < 250 ? new Length(Math.Round(DN1.Value * 3.5), "mm")
+                                            : new Length(DN1.Value * 3, "mm");
+        private Length GetPumpDimensionI() => DN1.Value < 125 ? new Length(Math.Round(DimH.Value / 2 + 12.5), "mm")
+                                            : DN1.Value < 175 ? new Length(Math.Round(DimH.Value / 2 + 25), "mm")
+                                            : DN1.Value < 250 ? new Length(Math.Round(DimH.Value / 2 + 57.5), "mm")
+                                            : new Length(Math.Round(DimH.Value / 2 + 75), "mm");
+        private Length GetPumpDimensionJ() => DN1.Value < 250 ? new Length(85, "mm")
+                                            : new Length(150, "mm");
+        private Length GetPumpDimensionO() => DN3.Value < 80 ? new Length(500 + Math.Ceiling(DN3.Value / 4) * 10, "mm")
+                                            : DN3.Value < 125 ? new Length(500 + Math.Ceiling(DN3.Value / 6) * 10, "mm")
+                                            : new Length(500 + Math.Ceiling(DN3.Value / 7) * 10, "mm");
+        private Length GetPumpDimensionP() => DN3.Value < 80 ? new Length(500 + Math.Ceiling(DN3.Value / 4) * 20, "mm")
+                                            : DN3.Value < 125 ? new Length(500 + Math.Ceiling(DN3.Value / 6) * 20, "mm")
+                                            : new Length(500 + Math.Ceiling(DN3.Value / 7) * 20, "mm");
+        private Length GetPumpDimensionR() =>
+            new Length(Math.Round((DN4.Value / 2 + 810)/5) * 5, "mm");
+        private Length GetPumpDimensionT() => DN3.Value < 80 ? new Length(Math.Ceiling(DN3.Value / 4) * 10 + DimS.Value + 300, "mm")
+                                            : DN3.Value < 125 ? new Length(Math.Ceiling(DN3.Value / 6) * 10 + DimS.Value + 300, "mm")
+                                            : new Length(Math.Ceiling(DN3.Value / 7) * 10 + DimS.Value + 300, "mm");
+        private Length GetMinimumLSWallDistanceX() =>
+            new Length(2 * DimK.Value + NumberOfPumps * DimH.Value + (NumberOfPumps - 1) * DimL.Value, "mm");
+        private Length GetMinimumLSWallDistanceY() =>
+            new Length((Math.Round((DimM.Value + DimE.Value + DimF.Value + DimG.Value + DimH.Value / 2) / 10) * 10) + 800, "mm");
+        private Length GetPumpDimensionZ() =>
+            new Length(DimK.Value + (NumberOfPumps - 1) * (DimH.Value + DimL.Value) + (DN4.Value + 50) * 11, "mm");
     }
-
+    
 }
